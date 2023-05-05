42750d1b8547669bfb4bbe380fdf94cb3e611526
markyl0ts <dev.marku@yahoo.com>
2023-05-05 22:44:06 +0800

    Update server name

--- include/functions.php
+++ include/functions.php
@@ -1,12 +1,12 @@
 <?php
 
 function OpenConnection(){
-    $serverName = "localhost";
+    $serverName = "192.168.254.121,1433";
     $connectionOptions = array("Database"=>"CashOut",
         "Uid"=>"couser", "PWD"=>"qqQQ11!!");
     $conn = sqlsrv_connect($serverName, $connectionOptions);
     if($conn == false)
-        die(FormatErrors(sqlsrv_errors()));
+        die(sqlsrv_errors());
 
     return $conn;
 }
